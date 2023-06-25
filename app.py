fffab3c17105b2956bdde11b637789c90c17d1c0
Ha Dinh <hadinh893@gmail.com>
2023-06-25 22:54:36 +0700

    update

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ import streamlit as st
 from utils import *
 
 
+print('reloaded')
 st.set_page_config(
     page_title='Polyp segmentation',
-    # page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded"
+    page_icon='🏥',
+    layout='wide',
+    initial_sidebar_state='expanded'
 )
+
 st.title('Polyp segmentation')
 
 img = st.file_uploader('Upload your endoscopic image')
+clicked = st.button('Continue')
 
-img = decode_and_normalize_image(img.read())
+if img is not None and clicked:
+    try:
+        img = decode_and_normalize_image(img.read())
+    except RuntimeError:
+        st.warning('Unsupported image file. Only jpeg and png are supported.')
+    
--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import torch
 from torchvision import io, transforms
+import matplotlib.pyplot as plt
+from model import *
+from torchview import draw_graph
 
-print(
-    list('ha')
-)
+
+net = UNet()
+state_dict = torch.load('trained_model/unet.pt', 'cpu')
+net.load_state_dict(state_dict)
+net.eval()
+
+x = torch.randn(1, 3, 256, 256)
