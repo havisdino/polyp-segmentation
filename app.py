d50b7646dee2a41cbdcd2d7400f79162d800e10b
Ha Dinh <hadinh893@gmail.com>
2023-06-30 14:43:22 +0700

    update the streamlit application

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from utils import *
+from model import UNet
 
 
-print('reloaded')
 st.set_page_config(
     page_title='Polyp segmentation',
     page_icon='🏥',
@@ -10,14 +10,38 @@ st.set_page_config(
     initial_sidebar_state='expanded'
 )
 
-st.title('Polyp segmentation')
 
-img = st.file_uploader('Upload your endoscopic image')
-clicked = st.button('Continue')
+@st.cache_resource
+def load_unet():
+    net =  UNet()
+    state_dict = torch.load('bin/unet128.pt', 'cpu')
+    net.load_state_dict(state_dict)
+    net.eval()
+    print('model loaded')
+    return net
+
+
+IMG_SIZE = (128, 128)
+net = load_unet()
+
+
+st.title('Polyp segmentation')
+c1, c2 = st.columns([0.3, 0.7])
+
+img = c1.file_uploader('Upload your endoscopic image', type=['png', 'jpg', 'jpeg'])
+clicked = c1.button('Continue')
 
 if img is not None and clicked:
     try:
-        img = decode_and_normalize_image(img.read())
+        img_org = decode_and_normalize_image(img.read())
     except RuntimeError:
-        st.warning('Unsupported image file. Only jpeg and png are supported.')
+        st.warning('Unsupported image file. Only jpg, jpeg and png are supported.')
     
+    img = transforms.functional.resize(img_org, IMG_SIZE, antialias=True)  
+    with torch.no_grad():
+        mask = net(img)
+    
+    mask_org = mask.unsqueeze(0)
+    mask_org = transforms.functional.resize(mask_org, img_org.shape[2:], antialias=True)
+    
+    c2.image(merge(img_org, mask_org))
--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
 import torch
 from torchvision import io, transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def decode_and_normalize_image(raw_byte):
     raw_byte = torch.ByteTensor(list(raw_byte))
-    img = io.decode_image(raw_byte)
-    return img[..., :3] / 255.0
+    img = io.decode_image(raw_byte).unsqueeze(0)[:, :3, ...]
+    return img / 255.0
+
+
+def merge(image, mask):
+    image = image.squeeze().permute(1, 2, 0).numpy()
+    prob_mask = mask.squeeze(0).numpy()
+    bin_mask = np.round(prob_mask)
+    heatmap = plt.cm.jet(prob_mask)[..., :3]
+    segmentation = (image + heatmap) * 0.5
+    return [image, segmentation, prob_mask, bin_mask]
+    
+    
